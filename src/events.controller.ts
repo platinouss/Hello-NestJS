3c7ee639a1666e202d81bd43cffbb096bf9bffd8
platinouss <platinouss@gmail.com>
2023-10-26 17:50:37 +0900

    feat: event controller 구현

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -10,9 +10,12 @@ import {
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
 import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
+
   @Get()
   findAll() {
     return [
@@ -23,21 +26,34 @@ export class EventsController {
 
   @Get(':id')
   findOne(@Param() id) {
-    return { id: 2, name: 'Second Event' };
+    return this.events.find((event) => event.id === parseInt(id));
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
   }
 
-  @Patch()
+  @Patch(':id')
   update(@Param('id') id, @Body() input: UpdateEventDto) {
-    input.address;
-    return id;
+    const index = this.events.findIndex((event) => event.id === parseInt(id));
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+    return this.events[index];
   }
 
-  @Delete()
+  @Delete(':id')
   @HttpCode(204)
-  delete() {}
+  delete(@Param('id') id) {
+    this.events = this.events.filter((event) => event.id !== parseInt(id));
+  }
 }
