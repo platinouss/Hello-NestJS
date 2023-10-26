39c2b7f6d8a0fd07f96bf7a0cedcab4ac1c7cf0b
platinouss <platinouss@gmail.com>
2023-10-26 17:57:53 +0900

    feat: docker-compose와 typeorm을 이용한 DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.20.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'hello-nestjs',
+    }),
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
