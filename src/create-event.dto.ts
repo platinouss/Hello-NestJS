23a475f4639e7781fde1b03d525952176c12379f
platinouss <platinouss@gmail.com>
2023-10-30 20:09:41 +0900

    feat: class-validation을 이용한 입력 값 검증

--- src/create-event.dto.ts
+++ src/create-event.dto.ts
@@ -1,6 +1,12 @@
+import { IsDateString, Length } from 'class-validator';
+
 export class CreateEventDto {
+  @Length(5, 255)
   name: string;
+  @Length(5, 255)
   description: string;
+  @IsDateString()
   when: string;
+  @Length(5, 255)
   address: string;
 }
