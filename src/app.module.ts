be30cd75e5ed7e9fcd1fc76ea0673b33962fa359
platinouss <platinouss@gmail.com>
2023-11-02 19:25:28 +0900

    feat: cascade 설정을 통한, N:M 관계 데이터 추가 로직 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { EventsModule } from './events/events.module';
 import { ConfigModule } from '@nestjs/config';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
+import { SchoolModule } from './school/school.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import ormConfigProd from './config/orm.config.prod';
         process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
     EventsModule,
+    SchoolModule,
   ],
   controllers: [AppController],
   providers: [AppService],
